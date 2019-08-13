57e453b80d927204b777efd45da7d76842bec5d1
Jessica Gomes <Jessica.Gomes@globalx.com.au>
2019-08-13 11:22:21 +1000

    Python program to sort unsorted names into .txt file

--- name-sorter.py
+++ name-sorter.py
@@ -1,7 +1,8 @@
-# Name-Sorter
+# Name-Sorter Program
 # Program to sort alphabetically last names, then by any given names and print to file 'sorted-names.txt'
+# Import string
+# Import os Library
 import os
-#import string
 path = str(os.getcwd()+'/')
 
 
@@ -17,6 +18,7 @@ def getData(filename):
 def sortData(list_data):
 
     list_data.sort(key=lambda x: x.split()[-1])  # Sort By Last Element Of List
+
     # Sort By Last Element And Then The Next
     #list_data.sort(key=lambda element: (element[-1], element[0]))
 
@@ -31,13 +33,9 @@ def saveValue(sort_value):
 
             for item in sort_value:
 
-                f.write("%s\n" % item)
+                f.write("%s\n" % item) #Write to file
 
         message = 'List of sorted names has been saved.'
-
-    #else:
-
-      #  message = 'List of names could not be saved.'
 
     return message
 
